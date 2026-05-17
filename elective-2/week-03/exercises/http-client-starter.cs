ef90c508269b7a25b0f616ff8741a943ae3193d3
ninalamo <nin.alamo@outlook.com>
2026-05-17 20:10:10 +0800

    removed old directories

--- elective-2/week-03/exercises/http-client-starter.cs
+++
@@ -1,9 +0,0 @@
-# HTTP HttpClient Starter
-
-```csharp
-using var client = new HttpClient();
-var response = await client.GetAsync("https://api.github.com/users/octocat");
-response.EnsureSuccessStatusCode();
-var body = await response.Content.ReadAsStringAsync();
-Console.WriteLine(body);
-```
